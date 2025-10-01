ce1585f912e3445d33084565a6bdf149dd1a1510
wilhelmges <wilhelmges@gmail.com>
2025-10-01 16:50:43 +0300

    reading store

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,9 @@
+import {getStore} from "@netlify/blobs";
+
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-  body: JSON.stringify({ message: 'Hello from server!' })
-  };
+    const store = getStore("main-store");
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: 'Hello from server! ' + await store.getJSON("v")})
+    };
 };
