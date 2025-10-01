bf9ff8f42ec292fec3a2f2dc1d4f62067ccbbf47
wilhelmges <wilhelmges@gmail.com>
2025-10-01 18:34:41 +0300

    викликати connectLambda(event) перед використанням getStore

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,7 @@
-import {getStore} from "@netlify/blobs";
+import {connectLambda, getStore} from "@netlify/blobs";
 
 exports.handler = async (event, context) => {
+    connectLambda(event);
     const store = getStore("main-store");
     return {
         statusCode: 200,
